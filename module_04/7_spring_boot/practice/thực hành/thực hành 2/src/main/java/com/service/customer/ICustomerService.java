f34593ccbc251414bd454d66a28c0ce0fc649efe
hoangvantuong0 <hoangvantuongdhqt@gmail.com>
2021-11-22 13:28:18 +0700

    up

---
+++ module_04/7_spring_boot/practice/thực hành/thực hành 2/src/main/java/com/service/customer/ICustomerService.java
@@ -0,0 +1,14 @@
+package com.service.customer;
+
+
+import com.model.Customer;
+import com.model.Province;
+import com.service.IGeneralService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+    Iterable<Customer> findAllByProvince(Province province);
+    Page<Customer> findAll(Pageable pageable);
+    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
+}
